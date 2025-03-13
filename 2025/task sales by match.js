3103325d71c9300904bb955eea742f6557bfa3ad
Oleksandr Cherkashyn <oleks.cherkashyn@gmail.com>
2025-03-13 18:46:22 +0200

    first preparation task

---
+++ 2025/task sales by match.js
@@ -0,0 +1,15 @@
+
+var n = 9
+var ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+var socksCount = []
+var count = 0
+    // Write your code here
+    var sum = 0;
+    var numberOfColors = Array(101);
+    ar.forEach((color) => {
+            sum += 1 & numberOfColors[color];
+            numberOfColors[color] = !numberOfColors[color];
+    })
+    
+    
+    
